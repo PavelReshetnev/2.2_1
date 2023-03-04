844cbdf4fc0ddb949ddd9edb1f6c8f1eb4020d21
PavelReshetnev <pasha8804088@gmail.com>
2023-03-04 11:15:35 +0700

    Initial commit

---
+++ ConsoleApp2.2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2._2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Student pavel = new Student();
+        pavel.Print();
+        pavel.Edit();
+    }
+}
---
+++ ConsoleApp2.2/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp2._2;
+
+public class Student
+{
+    public string secname = "Решетнев";
+    public string bd = "23.10.2004";
+    public int gr = 623;
+    public int[] score = new[] { 4, 3, 5, 5, 5 };
+
+    public void Edit()
+    {
+        Console.WriteLine("Желаете внести изменения? (да : 1; нет : 2)");
+        int vibor = Convert.ToInt32(Console.ReadLine());
+        switch (vibor)
+        {
+            case 1: 
+                Console.WriteLine("Фамилия");
+                secname = Console.ReadLine();
+                Console.WriteLine("Дата");
+                bd = Console.ReadLine();
+                Console.WriteLine("Группа");
+                gr = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Данные изменены\n");
+                    
+                goto case 3;
+            case 2:
+                Console.WriteLine("Хорошо");
+                break;
+            case 3:
+                Console.WriteLine("Проверка");
+                Console.WriteLine($"Фамилия: {secname}\nДата: {bd}\nГруппа: {gr}");
+                break;
+        }
+    }
+    public void Print() {
+        Console.WriteLine($"Фамилия: {secname}\nДата: {bd}\nГруппа: {gr}");
+        Console.WriteLine($"Успеваемость:{string.Join(", ", score)}");
+    }
+}
